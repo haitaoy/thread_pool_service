a6963b57ba42018cebf78267a77c5cf723373f70
yanghaitao <yanghaitao3@jd.com>
2016-06-07 21:04:05 +0800

    ThreadWrapper and thread poll service, thread with feature of enabled or disabled; service with: when pushing a task but get false, make a additive thread enabled.
    However,
    may be something wrong with constructor of ThreadWrapper, try to clearing...

--- thread_pool_service.h
+++ thread_pool_service.h
@@ -19,20 +19,35 @@
  *  Also, the {#keep_alive_time} is the maximum idle time of the threads newly started.
  */
 class ThreadPoolService {
+
+  typedef std::map<std::thread::id, ThreadWrapper>::value_type thread_type;
  public:
   ThreadPoolService(int core_pool_size,
                     int maximum_pool_size,
                     int keep_alive_time,
                     BlockingQueue<TaskWrapper> &task_queue)
-      : finished(false),
+      : finished_(false),
         core_pool_size_(core_pool_size),
         maximum_pool_size_(maximum_pool_size),
         keep_alive_time_(keep_alive_time),
         task_queue_(task_queue),
-        joiner(threads_) {
+        joiner_working_(working_pool_),
+        joiner_waiting_(waiting_pool_) {
     for (int i = 0; i < core_pool_size_; ++i) {
-      threads_.push_back(std::thread(&ThreadPoolService::DoWork, this));
+      auto boot = std::bind(&ThreadPoolService::DoWork, this);
+      ThreadWrapper worker(boot, true);
+      working_pool_.insert(thread_type(worker.get_id(), std::move(worker)));
     }
+    for (int i = core_pool_size_; i < maximum_pool_size_; ++i) {
+//      std::shared_ptr<std::thread> additive_thread(new std::thread(&ThreadPoolService::WaitAndWork, this));
+//      ThreadWrapper worker(additive_thread, false);
+//      waiting_pool_.insert(std::make_pair(worker.get_id(), worker));
+//      ThreadWrapper::ThreadWrapper(std::_Bind_helper<false, void (ThreadPoolService::*)(), ThreadPoolService*>::type, bool)
+    }
+
+//    finished_ = false;
+//    while (ThreadPoolSize() < maximum_pool_size_)
+//      cond_.notify_one();
   }
 
   template<typename F>
@@ -40,59 +55,102 @@ class ThreadPoolService {
     typedef typename std::result_of<F()>::type result_type;
     std::packaged_task<result_type()> task(std::move(f));
     std::future<result_type> future(task.get_future());
-    task_queue_.WaitAndPush(std::move(task));
+
+    Execute(TaskWrapper(std::move(task)));
     return future;
   }
 
  private:
   void DoWork() {
-    while (!finished) {
+    //WaitForStarting();
+
+    ThreadWrapper &current_worker = working_pool_[std::this_thread::get_id()];
+    while (!finished_ && current_worker.enabled_) {
+      TaskWrapper task;
+      if (task_queue_.Pop(task))
+        task();
+      else
+        std::this_thread::yield();
+    }
+  }
+
+  void WaitAndWork() {
+    //WaitForStarting();
+
+    ThreadWrapper &current_worker = waiting_pool_[std::this_thread::get_id()];
+    current_worker.WaitForEnabling();
+
+    {
+      std::lock_guard<std::mutex> lg(mutex_);
+      working_pool_.insert(thread_type(std::this_thread::get_id(), std::move(current_worker)));
+      waiting_pool_.erase(std::this_thread::get_id());
+    }
+
+    while (!finished_ && current_worker.enabled_) {
       std::shared_ptr<TaskWrapper> task;
-      if(GetTask(task)) {
-        if(task != nullptr)
+      if (GetTask(task)) {
+        if (task != nullptr)
           (*task)();
         else
           std::this_thread::yield();
       } else {
-        //thread exit safely
+        //suspend this thread
+        current_worker.enabled_ = false;
+        {
+          std::lock_guard<std::mutex> lg(mutex_);
+          waiting_pool_.insert(thread_type(std::this_thread::get_id(), std::move(current_worker)));
+          working_pool_.erase(std::this_thread::get_id());
+          current_worker.WaitForEnabling();
+        }
       }
     }
   }
 
-  void DoMoreWork(TaskWrapper&& task) {
-    task();
-    DoWork();
-  }
-
-  bool GetTask(std::shared_ptr<TaskWrapper>& task) {
+  bool GetTask(std::shared_ptr<TaskWrapper> &task) {
     task = task_queue_.WaitAndPop(keep_alive_time_);
-    if (task == nullptr && threads_.size() > core_pool_size_) //not atomic, continue later
+    if (task == nullptr && ThreadPoolSize() > core_pool_size_)
       return false;
     else
       return true;
   }
 
-  void Execute(TaskWrapper& task) {
-    if(!task_queue_.Push(std::move(task))) {
-      AddThread(task);
+  void Execute(TaskWrapper &&task) {
+    if (!task_queue_.Push(std::move(task))) {
+      AddThread();
     }
   }
 
-  void AddThread(TaskWrapper& task) {
+  void AddThread() {
     std::lock_guard<std::mutex> locked_guard(mutex_);
-    if(threads_.size() < maximum_pool_size_)
-      threads_.push_back(std::thread(&ThreadPoolService::DoMoreWork, this, std::move(task)));
+    if (working_pool_.size() < maximum_pool_size_ && waiting_pool_.size() > 0) {
+      ThreadWrapper& waken_worker = waiting_pool_.begin()->second;
+      waken_worker.enabled_ = true;
+      waken_worker.cond_->notify_one();
+    }
   }
 
-  bool finished;
+  int ThreadPoolSize() {
+    std::lock_guard<std::mutex> locked_guard(mutex_);
+    return working_pool_.size() + waiting_pool_.size();
+  }
+
+  void WaitForStarting() {
+    std::unique_lock<std::mutex> ul(mutex_);
+    cond_.wait(ul, [this] { return finished_; });
+  }
+
+  bool finished_;
   int core_pool_size_;
   int maximum_pool_size_;
   int keep_alive_time_;
   BlockingQueue<TaskWrapper> &task_queue_;
-  std::vector<std::thread> threads_;
-  ThreadsJoiner joiner;
+  std::map<std::thread::id, ThreadWrapper> working_pool_;
+  std::map<std::thread::id, ThreadWrapper> waiting_pool_;
+  ThreadsJoiner joiner_working_;
+  ThreadsJoiner joiner_waiting_;
 
   std::mutex mutex_;
+  std::condition_variable cond_;
 };
 
 #endif //THREAD_POOL_SERVICE_THREAD_POOL_SERVICE_H
--- thread_wrapper.h
+++ thread_wrapper.h
@@ -1,19 +1,84 @@
-
 #ifndef THREAD_POOL_SERVICE_THREAD_WRAPPER_H
 #define THREAD_POOL_SERVICE_THREAD_WRAPPER_H
 
+#include <map>
+
+struct ThreadWrapper {
+
+  struct ThreadBase {
+    virtual std::thread::id get_id() = 0;
+    virtual bool joinable() = 0;
+    virtual void join() = 0;
+    virtual ~ThreadBase() { };
+  };
+
+  template<typename F>
+  struct ThreadImpl: public ThreadBase {
+    F tf_;
+    std::thread t_;
+    ThreadImpl(F&& tf) : tf_(std::move(tf)), t_(std::thread(tf_)) { }
+    ThreadImpl(ThreadImpl&& other) : tf_(std::move(other.tf_)), t_(std::move(other.t_)) {
+      other.t_ = nullptr;
+    }
+
+    std::thread::id get_id() {
+      return t_.get_id();
+    }
+
+    bool joinable() {
+      return t_.joinable();
+    }
+
+    void join() {
+      t_.join();
+    }
+  };
+
+  ThreadWrapper() = default;
+  template<typename F>
+  ThreadWrapper(F &f, bool enabled) : thread_(new ThreadImpl<F>(std::move(f))), enabled_(enabled) { }
+  ThreadWrapper(ThreadWrapper &&other)
+      : enabled_(other.enabled_), thread_(std::move(other.thread_)), mutex_(std::move(other.mutex_)),
+        cond_(std::move(other.cond_)) { }
+
+  ThreadWrapper (const ThreadWrapper&) = delete;
+
+  void WaitForEnabling() {
+    std::unique_lock<std::mutex> locked_guard(*mutex_);
+    cond_->wait(locked_guard, [this] { return enabled_; });
+  }
+
+  std::thread::id get_id() {
+    return thread_->get_id();
+  }
+
+  bool joinable() {
+    return thread_->joinable();
+  }
+
+  void join() {
+    thread_->join();
+  }
+
+  bool enabled_;
+  std::unique_ptr<ThreadBase> thread_;
+  std::unique_ptr<std::mutex> mutex_;
+  std::unique_ptr<std::condition_variable> cond_;
+};
+
 class ThreadsJoiner {
  public:
-  explicit ThreadsJoiner(std::vector<std::thread> &threads) : threads_(threads) { }
+  explicit ThreadsJoiner(std::map<std::thread::id, ThreadWrapper> &threads) : threads_(threads) { }
   ~ThreadsJoiner() {
-    for (auto& item : threads_) {
-      if (item.joinable())
-        item.join();
+    for (auto &item : threads_) {
+      ThreadWrapper &thread = item.second;
+      if (thread.joinable())
+        thread.join();
     }
   }
 
  private:
-  std::vector<std::thread> &threads_;
+  std::map<std::thread::id, ThreadWrapper> &threads_;
 };
 
 #endif //THREAD_POOL_SERVICE_THREAD_WRAPPER_H
